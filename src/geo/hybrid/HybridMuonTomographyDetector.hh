047a91cde7861d176af3581e077a7dc487f456bb
Patrick Stowell <patrickstowell@Patricks-MacBook-Air.local>
2018-05-18 11:09:36 +0100

    Fixed warnings

--- src/geo/hybrid/HybridMuonTomographyDetector.hh
+++ src/geo/hybrid/HybridMuonTomographyDetector.hh
@@ -45,7 +45,6 @@
 #include "TF1.h"
 #include "TRandom3.h"
 #include "TVirtualFitter.h"
-#include "TStyle.h"
 #include "TSystem.h"
 #include "TStopwatch.h"
 
@@ -71,7 +70,7 @@ public:
   GeoObject* GetScintillatorBottom() { return fScintObjectBottom; };
 
   GeoObject* GetRPCTopX() { return fRPCTopX; };
-  GeoObject* GetRPCTopY() { return fRPCTopX; };
+  GeoObject* GetRPCTopY() { return fRPCTopY; };
 
   GeoObject* GetRPCBottomX() { return fRPCBottomX; };
   GeoObject* GetRPCBottomY() { return fRPCBottomY; };
@@ -111,6 +110,8 @@ public:
 
   /// Fit a set of XY points
   void GetMXC(G4double& m, G4double& me, G4double& c, G4double& ce, std::vector<G4double>& x, std::vector<G4double>& y, std::vector<G4double>& yerr);
+void ResetVariables();
+
 
 protected:
 
@@ -119,32 +120,45 @@ protected:
   HybridMuonTomographyDetector* fHybridDetector; ///< Pointer to associated detector medium
   std::vector<AWEDriftProcessor*> fDriftChamberProcs; ///< List of all drift processors
 
-  BristolRPCProcessor* fRPCProcTop; ///< Processor for the Bristol RPC Hits above
-  BristolRPCProcessor* fRPCProcBottom; ///< Processor for the Bristol RPC hits below
+  BristolRPCProcessor* fRPCProcTopX; ///< Processor for the Bristol RPC Hits above
+  BristolRPCProcessor* fRPCProcBottomX; ///< Processor for the Bristol RPC hits below
+  BristolRPCProcessor* fRPCProcTopY; ///< Processor for the Bristol RPC Hits above
+  BristolRPCProcessor* fRPCProcBottomY; ///< Processor for the Bristol RPC hits below
 
   SimpleScintillatorProcessor* fScintProcTop; ///< Scintillator trigger processor
   SimpleScintillatorProcessor* fScintProcBottom; ///< Scintillator trigger processor
 
-  int fMuonTimeIndex; ///< Time Ntuple Index
-  int fMuonMomXIndex; ///< MomX Ntuple Index
-  int fMuonMomYIndex; ///< MomY Ntuple Index
-  int fMuonMomZIndex; ///< MomZ Ntuple Index
-  int fMuonMomErrXIndex; ///< MomX Ntuple Index
-  int fMuonMomErrYIndex; ///< MomY Ntuple Index
-  int fMuonMomErrZIndex; ///< MomZ Ntuple Index
-  int fMuonPosXIndex; ///< PosX Ntuple Index
-  int fMuonPosYIndex; ///< PosY Ntuple Index
-  int fMuonPosZIndex; ///< PosZ Ntuple Index
-  int fMuonPosErrXIndex; ///< PosX Ntuple Index
-  int fMuonPosErrYIndex; ///< PosY Ntuple Index
-  int fMuonPosErrZIndex; ///< PosZ Ntuple Index
-  int fMuonPDGIndex;  ///< MPDG Ntuple Index
+  int fScintTopEIndex; ///< Time Ntuple Index
+  int fScintTopTIndex; ///< Time Ntuple Index
+  int fScintBottomEIndex; ///< Time Ntuple Index
+  int fScintBottomTIndex; ///< Time Ntuple Index
 
-  // Outputs
-  G4ThreeVector fMuonMom; 
-  G4ThreeVector fMuonMomErr;
-  G4ThreeVector fMuonPos;
-  G4ThreeVector fMuonPosErr;
+  G4double fScintTopE;
+  G4double fScintTopT;
+  G4double fScintBottomT;
+  G4double fScintBottomE;
+
+  std::vector<double> fRPCHits_XX;
+  std::vector<double> fRPCHits_XT;
+  std::vector<double> fRPCHits_XZ;
+  std::vector<double> fRPCHits_XE;
+
+  std::vector<double> fRPCHits_YY;
+  std::vector<double> fRPCHits_YT;
+  std::vector<double> fRPCHits_YZ;
+  std::vector<double> fRPCHits_YE;
+
+  std::vector<double> fDriftHits_XX;
+  std::vector<double> fDriftHits_XG;
+  std::vector<double> fDriftHits_XT;
+  std::vector<double> fDriftHits_XZ;
+  std::vector<double> fDriftHits_XE;
+
+  std::vector<double> fDriftHits_YY;
+  std::vector<double> fDriftHits_YG;
+  std::vector<double> fDriftHits_YT;
+  std::vector<double> fDriftHits_YZ;
+  std::vector<double> fDriftHits_YE;
 
 };
 
